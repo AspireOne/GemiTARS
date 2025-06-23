099ed84d653fbaf56929f3de942378fbcc0bb90e
Matěj Pešl <matejpesl1@gmail.com>
2025-06-23 22:34:28 +0200

    feat: add gemini live docs, other code changes

---
+++ docs/immediate_todo.md
@@ -0,0 +1,9 @@
+## Immediate TODO
+
+### Flow
+
+1. Listening for a
+
+### Gemini Live API
+
+- Implement audio to audio streaming (from microphone) and play the audio stream
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import os
+from typing import Any
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
-# Use env variable GEMINI_API_KEY.
 api_key = os.environ.get("GEMINI_API_KEY")
 if not api_key:
     raise RuntimeError(
@@ -13,17 +14,32 @@ if not api_key:
         "Please set it in your environment or in a .env file. "
         "If using a .env file, install python-dotenv."
     )
-client = genai.Client(api_key=api_key)
 
-def main() -> None:
-    print("Hello, world! Making a request to gemini...")
-    response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Explain how AI works in a few words",
-    config=types.GenerateContentConfig(
-        thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
-    )),
-    print(response[0].text)
+client = genai.Client(api_key=api_key)
+model = "gemini-2.5-flash"
+config: Any = {"response_modalities": ["TEXT"]}
+
+async def main() -> None:
+    print("Program started.")
+    async with client.aio.live.connect(model=model, config=config) as session:
+        print("Session started")
+    # TODO:
+    # - Listen for a hotword
+    # - When hotword is detected, start listening using Gemini Live API
+    # - Gemini Live API will stream back audio.
+    # - Play the audio
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
+    
+    
+    
+
+# Example request
+    # response = client.models.generate_content(
+    # model="gemini-2.5-flash",
+    # contents="Explain how AI works in a few words",
+    # config=types.GenerateContentConfig(
+    #     thinking_config=types.ThinkingConfig(thinking_budget=0) # Disables thinking
+    # )),
+    # print(response[0].text)
