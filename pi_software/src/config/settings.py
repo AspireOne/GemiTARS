6ab0dd89d40ceaab89d289bce8253262254de478
Matěj Pešl <matejpesl1@gmail.com>
2025-07-31 02:59:35 +0200

    chore(pi): increase AUDIO_PLAYBACK_QUEUE_SIZE (600 -> 1000)

--- pi_software/src/config/settings.py
+++ pi_software/src/config/settings.py
@@ -37,7 +37,7 @@ class Config:
     AUDIO_DTYPE = 'int16'
     AUDIO_CHANNELS = 1
     AUDIO_MIME_TYPE = "audio/pcm;rate=16000"
-    AUDIO_PLAYBACK_QUEUE_SIZE = 600
+    AUDIO_PLAYBACK_QUEUE_SIZE = 1000
 
     # Server Connection
     SERVER_HOST = os.getenv('SERVER_HOST', 'localhost')
