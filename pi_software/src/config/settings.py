8f0f8a8d8f7f28fde852f57c70afbe51e469015f
Matěj Pešl <matejpesl1@gmail.com>
2025-08-04 21:52:22 +0200

    chore(pi/button): change default button pin to 5

--- pi_software/src/config/settings.py
+++ pi_software/src/config/settings.py
@@ -63,5 +63,5 @@ class Config:
     
     # Button Settings
     BUTTON_ENABLED = _get_bool_env('BUTTON_ENABLED', True)  # Enable/disable button functionality
-    BUTTON_GPIO_PIN = int(os.getenv('BUTTON_GPIO_PIN', '6'))  # GPIO pin number for button
+    BUTTON_GPIO_PIN = int(os.getenv('BUTTON_GPIO_PIN', '5'))  # GPIO pin number for button
     BUTTON_DEBOUNCE_DELAY = float(os.getenv('BUTTON_DEBOUNCE_DELAY', '0.2'))  # Debounce delay in seconds
